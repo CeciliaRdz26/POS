1680ed9748d1e6b7db7d9fea41a23d3221a5ce41
Ana Cecilia Rodriguez Chavez <124944113+CeciliaRdz26@users.noreply.github.com>
2024-08-15 13:54:51 -0600

    bug

--- database/migrations/2024_06_01_165149_create_ventas_table.php
+++ database/migrations/2024_06_01_165149_create_ventas_table.php
@@ -8,21 +8,20 @@ class CreateVentasTable extends Migration
 {
     public function up()
     {
-        if (!Schema::hasTable('ventas')) {
-            Schema::create('ventas', function (Blueprint $table) {
-                $table->id('id_venta');
-                $table->foreignId('categoria_id')->constrained('categorias', 'id_categoria')->onDelete('cascade');
-                $table->foreignId('cliente_id')->constrained('clientes', 'id_cliente')->onDelete('cascade');
-                $table->foreignId('pago_id')->constrained('formasdepago', 'id')->onDelete('cascade');
-                $table->foreignId('vendedor_id')->constrained('vendedor', 'id')->onDelete('cascade');
-                $table->date('fecha_venta');
-                $table->double('subtotal');
-                $table->double('iva');
-                $table->double('total');
-                $table->enum('estatus', ['Activo', 'Inactivo'])->default('Activo');
-                $table->timestamps();
-            });
-        }
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id('id_venta');
+            $table->foreignId('categoria_id')->constrained('categorias', 'id_categoria')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes', 'id_cliente')->onDelete('cascade');
+            $table->foreignId('pago_id')->constrained('formasdepago', 'id')->onDelete('cascade');
+            $table->foreignId('vendedor_id')->constrained('vendedor', 'id')->onDelete('cascade');
+            $table->date('fecha_venta');
+            $table->double('subtotal');
+            $table->double('iva');
+            $table->double('total');
+            $table->enum('estatus', ['Activo', 'Inactivo'])->default('Activo');
+            $table->timestamps();
+        });
+        
     }
 
     public function down()
